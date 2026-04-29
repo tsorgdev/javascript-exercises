bd62d0e38a3649d082be124039b2a34b6c75bc40
tsorgdev <275804114+tsorgdev@users.noreply.github.com>
2026-04-28 20:04:11 -0700

    Complete JS Foundations 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(echoWord, repTimes) {
+    if (repTimes < 0){
+        return "ERROR";
+    }
+    let echo = "";
+    for (let i = 1; i <= repTimes; i++){
+        echo += echoWord;
+    }
+    return echo;
 };
 
 // Do not edit below this line
