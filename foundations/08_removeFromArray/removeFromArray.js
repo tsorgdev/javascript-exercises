47404dd4ba81b24b5e054692ef8e09fe1ae6209c
tsorgdev <275804114+tsorgdev@users.noreply.github.com>
2026-04-28 21:22:27 -0700

    Complete JS Foundations 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.from(arguments).slice(1);
+
+    for (const arg of args){
+        arr = arr.filter((item)=>!(item === arg));
+        
+    }
+    return arr;
 };
+
+removeFromArray([1,2,3,4],3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
