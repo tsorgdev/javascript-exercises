e610913bc1ce85903c13c2c893152ef827562dc5
tsorgdev <275804114+tsorgdev@users.noreply.github.com>
2026-04-29 12:01:17 -0700

    Complete JS Foundations 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    if (typeof (x) === "number" && typeof (y) === "number") {
+        if (Math.floor(x) === x && Math.floor(y) === y) {
+            if(x <= 0 || y <= 0){
+                return "ERROR";
+            }
+            if (x > y) {
+                let sum = 0;
+                for (let i = y; i <= x; i++) {
+                    sum += i;
+                }
+                return sum;
+            } else if (y > x) {
+                let sum = 0;
+                for (let i = x; i <= y; i++) {
+                    sum += i;
+                }
+                return sum;
+            } else {
+                return x;
+            }
+        }else{
+            return "ERROR";
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
