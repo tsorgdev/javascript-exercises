766aef77fb87100ac41192e3e9adf75bb64d3e4d
tsorgdev <275804114+tsorgdev@users.noreply.github.com>
2026-05-04 10:15:30 -0700

    Complete JS Foundations 14 Fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(target) {
+    if (+target < 0){
+        return "OOPS";
+    } else if (+target === 0){
+        return 0;
+    } else if (+target === 1){
+        return 1;
+    } else {
+        let prev = 0;
+        let curr = 1;
+        total = 0;
+        for (let i = 1; i < (+target); i++){
+            total = prev + curr;
+            prev = curr;
+            curr = total;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
