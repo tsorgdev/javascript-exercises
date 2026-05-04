2b43931b5752b125572dfb757de40c50a108828a
tsorgdev <275804114+tsorgdev@users.noreply.github.com>
2026-05-03 20:13:33 -0700

    Complete JS Foundations 12 Calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, number)=> (total + number), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, number)=> (total * number));
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+  if (pow === 0){
+    return 1;
+  } else if (pow === 1){
+    return base;
+  } else{
+    let total = base;
+    for (let i = 1; i < pow; i++){
+      total *= base;
+    }
+    return total;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+  for (let i = num; i > 1; i--){
+    num *= (i-1);
+  }
+	return num;
 };
 
 // Do not edit below this line
