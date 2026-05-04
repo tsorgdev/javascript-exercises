752024cefaa3daea1fca8c39e53f087473851976
tsorgdev <275804114+tsorgdev@users.noreply.github.com>
2026-05-03 21:10:23 -0700

    Complete JS Foundations 13 Palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replaceAll(/[^a-zA-Z0-9]/g,"");
+    if (str.length === 0) return false;
+    for (let i = 0; i < str.length; i++){
+        if (i >= (str.length - i - 1)) return true;
+        if (str.at(i) !== str.at(str.length - i - 1)) {
+            return false;
+        }
+    }
 };
-
+palindromes("A car, a man, a maraca.");
 // Do not edit below this line
 module.exports = palindromes;
