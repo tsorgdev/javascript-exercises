46ee7297606c6967a72310eb24abead56cc29e60
tsorgdev <275804114+tsorgdev@users.noreply.github.com>
2026-05-04 10:51:57 -0700

    Complete JS Foundations Find Oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    return arr.sort((a, b) => {
+        const date = new Date();
+        const year = date.getFullYear();
+        if (!("yearOfDeath" in a)) {
+            a["yearOfDeath"] = year;
+        }
+        if (!("yearOfDeath" in b)) {
+            b["yearOfDeath"] = year;
+        }
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    })[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
